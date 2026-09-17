59ba29a31dfe06cfa7f2dddce152732d04ddfc45
Anwesha.28 <sarkar.anwesha28@gmail.com>
2026-09-18 00:13:05 +0530

    Map_changes

--- frontend/src/components/DynamicMap.tsx
+++ frontend/src/components/DynamicMap.tsx
@@ -56,7 +56,7 @@ export default function DynamicMap({ submissions }: DynamicMapProps) {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       
       {submissions.map(s => {
